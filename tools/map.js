76a21b4fbc8c0ee4eb68cc196096fd43ffdb17e1
Irfan Ajanovic <irfanajanovic@gmail.com>
2017-12-16 09:46:33 -0500

    Updated map generation for tile-based collision/occlusion

--- tools/map.js
+++ tools/map.js
@@ -9,6 +9,32 @@ let output = {
   map: []
 };
 
+let setCollision = function(row,col,tileID,map){
+  let collisionMatrix = horizonsTile3x3.tiles[tileID].collision
+  let dimRow = collisionMatrix.length;
+  let dimCol = collisionMatrix[0].length;
+  for(let i = 0; i < dimRow; i++){
+    for(let j = 0; j < dimCol; j++){
+      if(collisionMatrix[i][j] == 1){
+        map[row-i][col+j].entity.collision = 1;
+      }
+    }
+  }
+}
+
+let setOcclusion = function(row,col,tileID,map){
+  let occlusionMatrix = horizonsTile3x3.tiles[tileID].occlusion
+  let dimRow = occlusionMatrix.length;
+  let dimCol = occlusionMatrix[0].length;
+  for(let i = 0; i < dimRow; i++){
+    for(let j = 0; j < dimCol; j++){
+      if(occlusionMatrix[i][j] == 1){
+        map[row-i][col+j].entity.occlusion = 1;
+      }
+    }
+  }
+}
+
 for(let i = 0; i < mapJSON.height; i++){
   output.map[i] = [];
   for(let j = mapJSON.width - 1; j >= 0; j--){
@@ -19,19 +45,21 @@ for(let i = 0; i < mapJSON.height; i++){
     if(tile.ground.id > 0){
       tile.ground.collission = horizonsTile1x1.tileproperties[tile.ground.id].collision;
     } else {
-      tile.ground.collission = false;
+      tile.ground.collission = 0;
     }
 
     tile.entity.id = mapJSON.layers[1].data[i*100+j];
     if(tile.entity.id > 0){
-      tile.entity.collision = horizonsTile3x3.tiles[tile.entity.id].collision
-      tile.entity.occlusion = horizonsTile3x3.tiles[tile.entity.id].occlusion
+      setCollision(i,j,tile.entity.id,output.map);
+      setOcclusion(i,j,tile.entity.id,output.map);
+      tile.entity.collision = horizonsTile3x3.tiles[tile.entity.id].collision[0][0];
+      tile.entity.occlusion = horizonsTile3x3.tiles[tile.entity.id].occlusion[0][0];
     } else {
-      tile.entity.collision = false;
-      tile.entity.occlusion = false;
+      tile.entity.collision = 0;
+      tile.entity.occlusion = 0;
     }
     output.map[i][j] = tile;
   }
 }
 
-fs.writeFile('map/output/map.json',JSON.stringify(output),'utf8',console.log("Done."));
+fs.writeFile('map/output/map.json',JSON.stringify(output),'utf8',console.log("Map Generation Completed."));
