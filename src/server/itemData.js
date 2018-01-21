d6fcf15b3db2ece9c04ee5b33f3cc167f5a9e575
Irfan Ajanovic <irfanajanovic@gmail.com>
2018-01-21 15:33:03 -0500

    Updated armor stats

--- src/server/itemData.js
+++ src/server/itemData.js
@@ -18,7 +18,7 @@ var itemData = {
     "subtype": 1,
     "name": "Chest Armor",
     "atk": 12,
-    "def": 100,
+    "def": 24,
     "levelReq": 1,
     "description": "An armor fit for a king. Many have spoken about it, but very few have seen it's shine."
   },
